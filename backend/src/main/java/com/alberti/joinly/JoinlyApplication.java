fd5d88498d4045214ebd4f978b735a16c15ce292
Juanfu224 <juanfu224@gmail.com>
2025-12-14 19:26:58 +0100

    Creada estructura backen con descomposicion de la bd y diagrama ER

---
+++ backend/src/main/java/com/alberti/joinly/JoinlyApplication.java
@@ -0,0 +1,13 @@
+package com.alberti.joinly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoinlyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JoinlyApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/alberti/joinly/JoinlyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alberti.joinly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoinlyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
