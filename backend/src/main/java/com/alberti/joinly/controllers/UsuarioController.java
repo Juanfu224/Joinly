63df45a95a11fc37e672a83486174678b91e4514
Juanfu224 <juanfu224@gmail.com>
2026-01-21 02:13:10 +0100

    eliminar importaciones innecesarias en UsuarioController

--- backend/src/main/java/com/alberti/joinly/controllers/UsuarioController.java
+++ backend/src/main/java/com/alberti/joinly/controllers/UsuarioController.java
@@ -3,7 +3,6 @@ package com.alberti.joinly.controllers;
 import com.alberti.joinly.dto.usuario.PreferenciasNotificacionDTO;
 import com.alberti.joinly.dto.usuario.UpdatePerfilRequest;
 import com.alberti.joinly.dto.usuario.UsuarioResponse;
-import com.alberti.joinly.security.UserPrincipal;
 import com.alberti.joinly.services.UsuarioService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
@@ -17,7 +16,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
