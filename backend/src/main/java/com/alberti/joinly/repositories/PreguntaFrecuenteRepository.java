31ed145f323740b33cfdec4aca42a0b937190503
Juanfu224 <juanfu224@gmail.com>
2026-02-10 09:23:02 +0100

    Creado repo preguntas frecuentes

---
+++ backend/src/main/java/com/alberti/joinly/repositories/PreguntaFrecuenteRepository.java
@@ -0,0 +1,18 @@
+package com.alberti.joinly.repositories;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.alberti.joinly.entities.faq.PreguntaFrecuente;
+import com.alberti.joinly.entities.enums.CategoriaFaq;
+
+@Repository
+public interface PreguntaFrecuenteRepository extends JpaRepository<PreguntaFrecuente, Long> {
+    List<PreguntaFrecuente> findByCategoria(CategoriaFaq categoria);
+
+    List<PreguntaFrecuente> findByActivoTrueOrderByOrdenAsc();
+    
+
+}
